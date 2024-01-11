fe6d6f0f928b41cf93e2b5b01c128332b967021d
stands30 <lancylotdsouza@gmail.com>
2024-01-11 17:10:58 +0530

    fetch years and expected return rate

--- investment_calculator.go
+++ investment_calculator.go
@@ -7,12 +7,18 @@ import (
 
 func main() {
 	const inflationRate = 6.5
-	//  investmentAmount := 1000
 	var investmentAmount float64
 	expectedReturnRate := 5.5
 	years := 10
+
 	fmt.Println("Investment Amount")
 	fmt.Scan(&investmentAmount)
+
+	fmt.Println("Expected Return Rate")
+	fmt.Scan(&expectedReturnRate)
+
+	fmt.Println("Years")
+	fmt.Scan(&years)
 
 	var futureValue = float64(investmentAmount) * math.Pow(1+expectedReturnRate/100, float64(years))
 	futureRealValue := futureValue * math.Pow(1+expectedReturnRate/100, float64(years))
